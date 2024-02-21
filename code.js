caa09138cfd7e2835155a075f0730c1adcee540c
meehal0203 <146681542+meehal0203@users.noreply.github.com>
2024-02-20 20:13:33 -0800

    adding more graphs

--- code.js
+++ code.js
@@ -1,111 +1,47 @@
-// let myMap = L.map("map", {
-//   center: [34.0522, -118.2437],
-//   zoom: 13
-// });
-
-// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-// }).addTo(myMap);
-
-
-// // define function to calcukate yearly change
-// function calculateChange(currentYear, previousYear) {
-//     if (previousYear === 0) {
-//       return "N/A";
-//     } else {
-//       const change = ((currentYear - previousYear) / previousYear) * 100;
-//       return change.toFixed(2) + "%";
-//     }
-//   }
-
-//   // Load GeoJSON data
-//   fetch("http://127.0.0.1:5500/Resources/crime_counts.json")
-//     .then(response => response.json())
-//     .then(precinctData => {
-//       // Initialize the map
-// let map = L.map("map", {
-//   center: [34.0522, -118.2437],
-//   zoom: 13
-// });
-// // const map = L.map("map").setView([34.0522, -118.2437], 10);
-//     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-//       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-//     }).addTo(map);
-
-// console.log(precinctData);
-// console.log(map);
-      
-//       // Define crime layer
-//       const crimeLayer = L.geoJson(precinctData, {
-//         // Access precinct and crime data properties
-//         onEachFeature: function (feature, layer) {
-//           const precinct = feature.properties.Precinct;
-//           const currentYear = feature.properties[2023]; // Access data for 2023
-//           const previousYear = feature.properties[2022]; // Access data for 2022
-//           const change = calculateChange(currentYear, previousYear);
-
-//           // Update popup content
-//           layer.bindPopup(`<b>Precinct: ${precinct}</b><br>2023 Crime Rate: ${currentYear}<br>Change from 2022: ${change}`);
-//         }
-//       });
-
-//       // Add crime layer to map
-//       crimeLayer.addTo(map);
-
-//       // Event listener for year selection
-//       document.getElementById("year-select").addEventListener("change", function (e) {
-//         const selectedYear = e.target.value;
-
-//         // Update crime layer data
-//         crimeLayer.eachLayer(function (layer) {
-//           layer.bindPopup(`<b>Precinct: ${layer.feature.properties.Precinct_Name}</b><br>${selectedYear} Crime Rate: ${layer.feature.properties[selectedYear]}`);
-//         });
-//       });
-//     });
-
-function calculateChange(currentYear, previousYear) {
-  if (previousYear === 0) {
-    return "N/A";
-  } else {
-    const change = ((currentYear - previousYear) / previousYear) * 100;
-    return change.toFixed(2) + "%";
-  }
-}
-// Load GeoJSON data
-fetch("http://127.0.0.1:5500/Resources/crime_counts.json")
-  .then(response => response.json())
-  .then(precinctData => {
-    // Initialize the map
 let map = L.map("map", {
-center: [34.0522, -118.2437],
-zoom: 13
+  center: [34.0522, -118.2437],
+  zoom: 10
 });
-// const map = L.map("map").setView([34.0522, -118.2437], 10);
-  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(map);
-console.log(precinctData);
-console.log(map);
-    // Define crime layer
-    const crimeLayer = L.geoJson(precinctData, {
-      // Access precinct and crime data properties
-      onEachFeature: function (feature, layer) {
-        const precinct = feature.properties.Precinct;
-        const currentYear = feature.properties[2023]; // Access data for 2023
-        const previousYear = feature.properties[2022]; // Access data for 2022
-        const change = calculateChange(currentYear, previousYear);
-        // Update popup content
-        layer.bindPopup(`<b>Precinct: ${precinct}</b><br>2023 Crime Rate: ${currentYear}<br>Change from 2022: ${change}`);
-      }
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+let url = "http://127.0.0.1:5500/Resources/crime_counts.json";
+d3.json(url)
+  .then(precinctData => {
+    // Create an empty array to store the markers
+    const markers = [];
+
+    // Iterate over the keys of the "Precinct" object
+    Object.keys(precinctData.Precinct).forEach((key, index) => {
+      const precinctName = precinctData.Precinct[key];
+      const year = precinctData.Year[key];
+      const totalCrimes = precinctData["Total Crimes"][key];
+      const latitude = precinctData.Lat[key];
+      const longitude = precinctData.Lng[key];
+
+      // Create a marker for each precinct and bind a popup with the crime data
+      const marker = L.marker([latitude, longitude]).addTo(map);
+      marker.bindPopup(`<b>Precinct: ${precinctName}</b><br>Year: ${year}<br>Total Crimes: ${totalCrimes}`);
+
+      // Add the marker to the markers array
+      markers.push(marker);
     });
-    // Add crime layer to map
-    crimeLayer.addTo(map);
+
     // Event listener for year selection
-    document.getElementById("year-select").addEventListener("change", function (e) {
-      const selectedYear = e.target.value;
-      // Update crime layer data
-      crimeLayer.eachLayer(function (layer) {
-        layer.bindPopup(`<b>Precinct: ${layer.feature.properties.Precinct_Name}</b><br>${selectedYear} Crime Rate: ${layer.feature.properties[selectedYear]}`);
+    const yearSelect = document.getElementById("year-select");
+    if (yearSelect) {
+      yearSelect.addEventListener("change", function (e) {
+        const selectedYear = parseInt(e.target.value);
+        // Show markers for the selected year and hide markers for other years
+        markers.forEach(marker => {
+          const markerYear = parseInt(marker.getPopup().getContent().split("<br>")[1].split(": ")[1]);
+          if (markerYear === selectedYear) {
+            marker.addTo(map);
+          } else {
+            map.removeLayer(marker);
+          }
+        });
       });
-    });
+    }
   });
